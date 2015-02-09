d006395a5c8ae70ee204609b73983192a38f607e
M. A. Alim Mukul <mukul.sustcse@gmail.com>
2015-02-10 01:39:00 +0600

    DONE

--- gitTest/src/gittest/GitTest.java
+++ gitTest/src/gittest/GitTest.java
@@ -17,6 +17,7 @@ public class GitTest {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("it works");
+        System.out.println(":)");
     }
     
 }
